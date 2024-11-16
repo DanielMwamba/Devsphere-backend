7914fa18fbd37505d67e4435943dfc7474ee2758
Daniel Mwamba <danmuamba81@gmail.com>
2024-11-16 23:56:05 +0100

    fix: add other origin

--- src/index.js
+++ src/index.js
@@ -9,23 +9,32 @@ const commentRouter = require("./routes/comment.routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configuration des options CORS
-const corsOption = {
-  origin: "https://kongodev.vercel.app",
+// Liste des origines autorisées
+const allowedOrigins = [
+  "https://kongodev.vercel.app",
+  "https://kongodev.netlify.app",
+];
+
+// Middleware CORS avec vérification dynamique
+app.use(cors({
+  origin: function (origin, callback) {
+    // Vérifier si l'origine de la requête est dans la liste des origines autorisées ou est undefined (cas des requêtes internes)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
-};
+}));
 
-// Middleware CORS global
-app.use(cors(corsOption));
+// Middleware pour les requêtes préliminaires (OPTIONS)
+app.options("*", cors());
 
-// Middleware pour gérer les requêtes préliminaires (OPTIONS)
-app.options("*", cors(corsOption));
-
-// En-têtes de réponse pour toutes les requêtes
+// En-têtes de réponse supplémentaires
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://kongodev.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
